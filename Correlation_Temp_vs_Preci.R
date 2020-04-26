5c9b10e7df604d2b380d8ac7e6517e47aa39031f
Pablo E. Gutierrez Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-04-26 00:28:27 -0600

    temperature and precipitation

---
+++ Correlation_Temp_vs_Preci.R
@@ -0,0 +1,13 @@
+
+
+
+
+#--------------------------------------------
+# Correlation Temperature vs Precipitation 
+# 25 Apr 2020
+#PEGF
+#--------------------------------------------
+#
+
+correlations<- read.csv("Temp_vs_Preci.csv")
+correlations
