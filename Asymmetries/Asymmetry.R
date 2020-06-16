28781e9a34e26c7c8061728fc9d91fc89c0faf94
Pablo E. Gutierrez Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-06-15 22:21:06 -0600

    FA Index

--- Asymmetries/Asymmetry.R
+++ Asymmetries/Asymmetry.R
@@ -25,13 +25,13 @@ shapiro.test(ValueFWAF)
 V1 <- 1/sqrt(ValueFWAF)
 shapiro.test(V1)
 
-fit2 <- lmerTest::lmer(V1 ~ Side*Individual + (1|Individual) + (1|Individual:Side), 
+mod1 <- lmerTest::lmer(V1 ~ Side*Individual + (1|Individual) + (1|Individual:Side), 
                        data= FWAF.frm, REML= FALSE, control =lmerControl(check.conv.singular = .makeCC(action = "ignore",  tol = 1e-4)))
-anova(fit2)
-mse(fit2)
-var(resid(fit2))*99/90
-summary(fit2)
-rand(fit2)
+anova(mod1)
+mse(mod1)
+var(resid(mod1))*99/90
+summary(mod1)
+rand(mod1)
 
 
 # First Wing Length Female ------------------------------------------------
@@ -44,13 +44,13 @@ FWLF.frm
 shapiro.test(ValuesFWLF)
 boxplot(ValuesFWLF~Side,data=FWLF.frm)
 
-fit2 <- lmerTest::lmer(ValuesFWLF ~ Side*Individual + (1|Individual) + (1|Individual:Side), 
+mod2 <- lmerTest::lmer(ValuesFWLF ~ Side*Individual + (1|Individual) + (1|Individual:Side), 
                    data= FWLF.frm, REML= FALSE, control =lmerControl(check.conv.singular = .makeCC(action = "ignore",  tol = 1e-4)))
-anova(fit2)
-mse(fit2)
-var(resid(fit2))*99/90
-summary(fit2)
-rand(fit2)
+anova(mod2)
+mse(mod2)
+var(resid(mod2))*99/90
+summary(mod2)
+rand(mod2)
 
 
 # First Wing Area Male --------------------------------------------------
@@ -65,27 +65,33 @@ shapiro.test(ValueFWAM)
 V3<-1/(ValueFWAM)
 shapiro.test(V3)
 
-fit2 <- lmerTest::lmer(V3 ~ Side*Individual + (1|Individual) + (1|Individual:Side), 
+mod3 <- lmerTest::lmer(V3 ~ Side*Individual + (1|Individual) + (1|Individual:Side), 
                        data= FWAM.frm, REML= FALSE, control =lmerControl(check.conv.singular = .makeCC(action = "ignore",  tol = 1e-4)))
-anova(fit2)
-mse(fit2)
-var(resid(fit2))*99/90
-summary(fit2)
-rand(fit2)
+anova(mod3)
+mse(mod3)
+var(resid(mod3))*99/90
+summary(mod3)
+rand(mod3)
 
 
 
 # First Wing Length Male ------------------------------------------------
 # FWAM = First Wing Length Male
 
-FWLM.frm=read.csv("FirstWLengthMale.csv")
+FWLM.frm=read.csv("Asymmetries/FirstWLengthMale.csv")
 attach(FWLM.frm)
 FWLM.frm
 
 shapiro.test(ValueFWLM)
 
-D.mod=aov(ValueFWLM ~ Side * Individual + (1 | Individual) ,data=FWLM.frm)
-summary(D.mod)
+mod4 <- lmerTest::lmer(ValueFWLM ~ Side*Individual + (1|Individual) + (1|Individual:Side), 
+                       data= FWLM.frm, REML= FALSE, control =lmerControl(check.conv.singular = .makeCC(action = "ignore",  tol = 1e-4)))
+anova(mod4)
+mse(mod4)
+var(resid(mod4))*99/90
+summary(mod4)
+rand(mod4)
+
 
 ##### Hind Wing #####
 
