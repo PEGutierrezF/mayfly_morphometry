79a0968d40f0ec076aeb93a5e3610042af525ab3
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-08-10 13:12:59 -0600

    confirming Data

--- Asymmetries/Asymmetry.R
+++ Asymmetries/Asymmetry.R
@@ -31,9 +31,11 @@ FWAF.frm
 head(FWAF.frm)
 
 shapiro.test(ValueFWAF)
+V1<-1/(ValueFWAF)
+shapiro.test(V1)
 boxplot(ValueFWAF~Side,data=FWAF.frm)
 
-mod1 <- lmerTest::lmer(ValueFWAF ~ Side*Individual + (1|Individual) + (1|Individual:Side), 
+mod1 <- lmerTest::lmer(V1 ~ Side*Individual + (1|Individual) + (1|Individual:Side), 
                        data= FWAF.frm, REML= FALSE, control =lmerControl(check.conv.singular = .makeCC(action = "ignore",  tol = 1e-4)))
 anova(mod1)
 mse(mod1)
